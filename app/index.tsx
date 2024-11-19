40c7c8b9e84b0fb57656f30a8d0d7864166f7847
lima8002 <57651553+lima8002@users.noreply.github.com>
2024-11-19 12:54:33 +0000

    initial environment configuration, update app layout with header and footer components, and integrate SVG support for React Native. Update dependencies to include font libraries and SVG handling.

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,43 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
+import Icon from "@/assets/images/wordle-icon.svg";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Icon width={100} height={100} />
+        <Text style={styles.title}>Wordle</Text>
+        <Text style={styles.text}>Get 6 chances to guess a 5-letter word.</Text>
+      </View>
+
+      <View>
+        <Text style={styles.text}>Start a new game</Text>
+      </View>
+
+      <View style={styles.footer}>
+        <Text style={styles.text}>Made by Youtube channel</Text>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    paddingHorizontal: 50,
+    gap: 40,
+  },
+  header: {
+    alignItems: "center",
+    gap: 10,
+  },
+  title: {
+    fontSize: 40,
+  },
+  text: {
+    fontSize: 26,
+    textAlign: "center",
+  },
+  footer: {},
+});
