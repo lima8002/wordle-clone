032ba24d7bf7d74ba38536c27403f2092b4f9ee5
lima8002 <57651553+lima8002@users.noreply.github.com>
2024-11-19 15:06:17 +0000

    Add fontLayout game page; update Index component with navigation and styles for new buttons and footer.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,35 @@
 import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
+import {
+  useFonts,
+  FrankRuhlLibre_800ExtraBold,
+  FrankRuhlLibre_500Medium,
+  FrankRuhlLibre_900Black,
+} from "@expo-google-fonts/frank-ruhl-libre";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  return <Stack />;
+  const [fontsLoaded, error] = useFonts({
+    FrankRuhlLibre_800ExtraBold,
+    FrankRuhlLibre_500Medium,
+    FrankRuhlLibre_900Black,
+  });
+
+  useEffect(() => {
+    if (fontsLoaded || error) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, error]);
+
+  if (!fontsLoaded && !error) {
+    return null;
+  }
+
+  return (
+    <Stack>
+      <Stack.Screen name="index" options={{ headerShown: false }} />
+    </Stack>
+  );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,7 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import Icon from "@/assets/images/wordle-icon.svg";
+import { Link } from "expo-router";
+import { format } from "date-fns";
 
 export default function Index() {
   return (
@@ -10,12 +12,28 @@ export default function Index() {
         <Text style={styles.text}>Get 6 chances to guess a 5-letter word.</Text>
       </View>
 
-      <View>
-        <Text style={styles.text}>Start a new game</Text>
+      <View style={styles.menu}>
+        <Link href={"/game"} asChild style={styles.btn}>
+          <TouchableOpacity style={styles.primaryItem}>
+            <Text style={[styles.btnText, styles.primaryText]}>Play</Text>
+          </TouchableOpacity>
+        </Link>
+
+        <TouchableOpacity style={styles.btn}>
+          <Text style={styles.btnText}>Log in</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.btn}>
+          <Text style={styles.btnText}>Subscribe</Text>
+        </TouchableOpacity>
       </View>
 
       <View style={styles.footer}>
-        <Text style={styles.text}>Made by Youtube channel</Text>
+        <Text style={styles.footerDate}>
+          {format(new Date(), "MMMM d, yyyy")}
+        </Text>
+        <Text style={styles.footerText}>No. 1151</Text>
+        <Text style={styles.footerText}>Edited by Youtube channel</Text>
       </View>
     </View>
   );
@@ -34,10 +52,47 @@ const styles = StyleSheet.create({
   },
   title: {
     fontSize: 40,
+    fontFamily: "FrankRuhlLibre_800ExtraBold",
   },
   text: {
     fontSize: 26,
     textAlign: "center",
   },
-  footer: {},
+  menu: {
+    alignItems: "center",
+    justifyContent: "center",
+    gap: 10,
+  },
+  footer: {
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  footerText: {
+    fontSize: 14,
+  },
+  footerDate: {
+    fontSize: 14,
+    fontWeight: "bold",
+  },
+  btn: {
+    justifyContent: "center",
+    borderRadius: 30,
+    alignItems: "center",
+    borderColor: "#000",
+    borderWidth: 1,
+    width: "60%",
+    maxWidth: 200,
+  },
+  btnText: {
+    padding: 14,
+    fontSize: 16,
+    fontWeight: "semibold",
+    color: "#333",
+  },
+  primaryItem: {
+    backgroundColor: "#000",
+  },
+  primaryText: {
+    color: "#fff",
+  },
 });
