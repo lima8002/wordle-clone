00df0b5c8352b692c2277f840882316af56c45b9
lima8002 <57651553+lima8002@users.noreply.github.com>
2024-11-21 14:08:56 +0000

    Implement login functionality with OAuth support for Google, Apple, and Facebook. Enhance UI with a structured layout, input fields, and buttons for each authentication method.

--- app/login.tsx
+++ app/login.tsx
@@ -1,14 +1,168 @@
-import { StyleSheet, Text, View } from "react-native";
+import {
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import React from "react";
+import { useRouter } from "expo-router";
+import { useOAuth } from "@clerk/clerk-expo";
+import { defaultStyles } from "@/constants/Styles";
+import { Colors } from "@/constants/Colors";
+import { Ionicons } from "@expo/vector-icons";
+
+enum Strategy {
+  Google = "oauth_google",
+  Apple = "oauth_apple",
+  Facebook = "oauth_facebook",
+}
 
 const Page = () => {
+  const router = useRouter();
+
+  const { startOAuthFlow: googleAuth } = useOAuth({
+    strategy: Strategy.Google,
+  });
+  const { startOAuthFlow: appleAuth } = useOAuth({ strategy: Strategy.Apple });
+  const { startOAuthFlow: facebookAuth } = useOAuth({
+    strategy: Strategy.Facebook,
+  });
+
+  const onSelectAuth = async (strategy: Strategy) => {
+    const selectedAuth = {
+      [Strategy.Google]: googleAuth,
+      [Strategy.Apple]: appleAuth,
+      [Strategy.Facebook]: facebookAuth,
+    }[strategy];
+
+    try {
+      const { createdSessionId, setActive } = await selectedAuth();
+
+      if (createdSessionId) {
+        setActive!({ session: createdSessionId });
+        router.back();
+      }
+    } catch (error) {
+      console.error("Error starting OAuth flow", error);
+    }
+  };
+
   return (
-    <View>
-      <Text>Page</Text>
-    </View>
+    <ScrollView style={styles.container}>
+      <Text style={styles.header}>Log in or create an account</Text>
+      <Text style={styles.subText}>
+        By continuing, you agree to the Terms of Sale, Terms of Service and
+        Privacy Policy.
+      </Text>
+
+      <Text style={styles.inputLabel}>Email</Text>
+      <TextInput style={styles.input} placeholder="Email" />
+
+      <TouchableOpacity style={defaultStyles.btn}>
+        <Text style={defaultStyles.btnText}>Continue</Text>
+      </TouchableOpacity>
+
+      <View style={styles.separatorView}>
+        <View style={styles.separatorLine} />
+        <Text style={styles.separator}>or</Text>
+        <View style={styles.separatorLine} />
+      </View>
+
+      <View style={{ gap: 20 }}>
+        <TouchableOpacity
+          style={styles.btnOutline}
+          onPress={() => onSelectAuth(Strategy.Google)}
+        >
+          <Ionicons name="logo-google" size={24} style={styles.btnIcon} />
+          <Text style={styles.btnOutlineText}>Continue with Google</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.btnOutline}
+          onPress={() => onSelectAuth(Strategy.Apple)}
+        >
+          <Ionicons name="logo-apple" size={24} style={styles.btnIcon} />
+          <Text style={styles.btnOutlineText}>Continue with Apple</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.btnOutline}
+          onPress={() => onSelectAuth(Strategy.Facebook)}
+        >
+          <Ionicons name="logo-facebook" size={24} style={styles.btnIcon} />
+          <Text style={styles.btnOutlineText}>Continue with Facebook</Text>
+        </TouchableOpacity>
+      </View>
+    </ScrollView>
   );
 };
 
 export default Page;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#fff",
+    flex: 1,
+    paddingHorizontal: 40,
+  },
+  header: {
+    fontSize: 20,
+    fontWeight: "bold",
+    marginBottom: 20,
+    paddingTop: 30,
+    paddingBottom: 20,
+    textAlign: "center",
+  },
+  subText: {
+    fontSize: 14,
+    color: "#4f4f4f",
+    textAlign: "center",
+    marginBottom: 30,
+  },
+  inputLabel: {
+    paddingBottom: 5,
+    fontWeight: 500,
+  },
+  input: {
+    height: 50,
+    borderColor: "#ccc",
+    borderWidth: 1,
+    borderRadius: 4,
+    paddingHorizontal: 10,
+    marginBottom: 15,
+  },
+  separatorView: {
+    flexDirection: "row",
+    gap: 10,
+    alignItems: "center",
+    marginVertical: 30,
+  },
+  separator: {
+    fontSize: 16,
+    color: Colors.light.gray,
+  },
+  separatorLine: {
+    borderBottomColor: "#000",
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    flex: 1,
+  },
+  btnOutline: {
+    backgroundColor: "#fff",
+    borderWidth: 1,
+    borderColor: "#000",
+    height: 50,
+    borderRadius: 4,
+    justifyContent: "center",
+    alignItems: "center",
+    paddingHorizontal: 10,
+    flexDirection: "row",
+  },
+  btnOutlineText: {
+    color: "#000",
+    fontSize: 16,
+    fontWeight: "500",
+  },
+  btnIcon: {
+    paddingRight: 10,
+  },
+});
